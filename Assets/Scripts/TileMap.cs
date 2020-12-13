73268893540e5155631bcf89f7294e1057da7b30
Zach North <zrnorth@gmail.com>
2020-12-12 23:36:44 -0500

    Created tile prefabs and assigned to TileTypes to map

---
+++ Assets/Scripts/TileMap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour
+{
+    [SerializeField]
+    int _mapSizeX, _mapSizeY;
+    [SerializeField]
+    TileType[] _tileTypes; // Contains list of the type of tiles
+    int[,] tiles;         // map of tile#s to their type
+
+    private void Start()
+    {
+        tiles = new int[_mapSizeX, _mapSizeY];
+        // Zero out all the tiles
+        for (int x = 0; x < _mapSizeX; x++)
+        {
+            for (int y = 0; y < _mapSizeY; y++)
+            {
+                tiles[x, y] = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TileType.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TileType
+{
+    public string _name;
+    public bool _isPassable;
+
+    public GameObject _tilePrefab;
+}
