2b8b6a5aa8424c25bc016b215d04f56fe09e7d93
Zach North <zrnorth@gmail.com>
2020-12-13 01:45:20 -0500

    Added TileMap a* graph generation

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class TileMap : MonoBehaviour
 {
+    // For pathfinding
+    class Node
+    {
+        public int x, y;
+        public List<Node> neighbors; // Not an array lmao
+        public Node(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            neighbors = new List<Node>();
+        }
+        public string GetPosString()
+        {
+            return "(" + x + ", " + y + ")";
+        }
+        public override string ToString()
+        {
+            string str = GetPosString() + " -> [";
+            foreach (Node neighbor in neighbors)
+            {
+                str += neighbor.GetPosString() + ", ";
+            }
+            str += "]";
+            return str;
+        }
+    }
+
+    // Game modifiers
     [SerializeField]
     int _mapSizeX, _mapSizeY;
     [SerializeField]
@@ -15,12 +43,16 @@ public class TileMap : MonoBehaviour
     [SerializeField]
     Camera _mainCamera;
 
+    // Class vars
     private GameObject _selectedUnit;
     int[,] tiles;         // map of tile#s to their type
+    Node[,] _graph;       // Nodes for pathfinding
+
 
     private void Start()
     {
         GenerateTiles();
+        GeneratePathfindingGraph();
         GenerateMapVisuals();
         // Move the camera to the center of the battlefield
         _mainCamera.transform.position = new Vector3((_mapSizeX - 1) / 2f, (_mapSizeY - 1) / 2f, -10);
@@ -55,6 +87,45 @@ public class TileMap : MonoBehaviour
         tiles[7, 7] = 2; // Water
     }
 
+    private void GeneratePathfindingGraph()
+    {
+        _graph = new Node[_mapSizeX, _mapSizeY];
+        // Initialize the _graph array
+
+        for (int x = 0; x < _mapSizeX; x++)
+        {
+            for (int y = 0; y < _mapSizeY; y++)
+            {
+                _graph[x, y] = new Node(x, y);
+            }
+        }
+
+        for (int x = 0; x < _mapSizeX; x++)
+        {
+            for (int y = 0; y < _mapSizeY; y++)
+            {
+                Node curr = _graph[x, y];
+                // No diagonal movement
+                if (y < _mapSizeY - 1)
+                {
+                    curr.neighbors.Add(_graph[x, y + 1]);
+                }
+                if (y > 0)
+                {
+                    curr.neighbors.Add(_graph[x, y - 1]);
+                }
+                if (x < _mapSizeX - 1)
+                {
+                    curr.neighbors.Add(_graph[x + 1, y]);
+                }
+                if (x > 0)
+                {
+                    curr.neighbors.Add(_graph[x - 1, y]);
+                }
+            }
+        }
+    }
+
     private void GenerateMapVisuals()
     {
         for (int x = 0; x < _mapSizeX; x++)
@@ -78,16 +149,24 @@ public class TileMap : MonoBehaviour
     // Helper to pass in a tile position and return a world coord of the tile position.
     public Vector3 TileCoordToWorldCoord(int x, int y)
     {
+        // TODO implement
         return new Vector3(x, y, 0);
     }
 
     // TODO: add unit selection
-    public void MoveSelectedUnitTo(int x, int y)
+    // jump directly to (x, y)
+    public void TeleportSelectedUnitTo(int x, int y)
     {
         Unit unit = _selectedUnit.GetComponent<Unit>();
         // Set position both in the tilemap and worldmap. These might coincidentally be the same,
         // but lets assume they aren't.
         unit.SetTileMapPosition(x, y);
         unit.transform.position = TileCoordToWorldCoord(x, y);
+    }
+
+    // Pathfind to (x, y)
+    public void MoveSelectedUnitTo(int x, int y)
+    {
+        TeleportSelectedUnitTo(x, y);
     }
 }
